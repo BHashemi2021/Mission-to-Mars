6ed3dce594cf1ceba936af813710b6cfd54feed7
BHashemi2021 <bhashemi2021@gmail.com>
2021-07-05 00:06:35 -0400

    adding the completed files and images.

--- Mars_Scraping/app.py
+++ Mars_Scraping/app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase" 
 mongo = PyMongo(app)
 
 
@@ -21,14 +21,11 @@ def index():
 def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
+   # Now that we've gathered new data, we need to update the database
    mars.update({}, mars_data, upsert=True)
    return redirect('/', code=302)
 
 
-# Now that we've gathered new data, we need to update the database
-mars.update({}, mars_data, upsert=True)
-
-
 # The final bit of code we need for Flask is to tell it to run. 
 if __name__ == "__main__":
-   app.run()
+   app.run(debug=True)
